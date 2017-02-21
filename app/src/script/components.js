994e016a86336f8e39b3c54ecd9d877b8aa83b62
sven-zo <0923782@hr.nl>
2017-02-21 13:32:10 +0100

    Added some comments.

--- app/src/script/components.js
+++ app/src/script/components.js
@@ -1,9 +1,11 @@
 //JS file for all the Vue components.
 
+//Homepage
 const home = {
     template: '<p>Home</p>'
 };
 
+//Detail screen for the fish
 const fishDetail = {
     template: '<p>Fish detail</p>'
 };
