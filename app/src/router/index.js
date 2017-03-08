1b7adb68c6b7ee9c2012eab9715e8b81caa6aaf3
sven-zo <svenzo@outlook.com>
2017-03-08 12:03:29 +0100

    Fixed missing 'Home.vue'

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home from '@/components/home/Home'
 import FishDetail from '@/components/fish/FishDetail'
 import Settings from '@/components/settings/Settings'
 import AddRod from '@/components/addrod/AddRod'
